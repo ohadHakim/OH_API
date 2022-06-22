9b8eaebcb03f0754cb4c8036fc1d32b3d003a750
Ohad Hakim <hakimohad@gmail.com>
2022-06-22 23:47:09 +0300

    adding comments

--- routes/customers.js
+++ routes/customers.js
@@ -4,6 +4,7 @@ const cm = require("../controllers/customers");
 
 /* POST new customer */
 router.post("/add", cm.addCustomer);
+/*GET customer by id*/
 router.get("/:id", cm.getCustomer);
 
 module.exports = router;
